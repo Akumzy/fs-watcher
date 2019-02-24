2e6cfa869da4c76323762fc24ebce3614edcec2d
Akuma Isaac Akuma <Akumanewton@gmail.com>
2019-02-24 16:28:42 +0100

    caching downloaded binary

--- download.ts
+++ download.ts
@@ -1,8 +1,12 @@
-import { downloadBinary, binName, binPath } from './utils'
-import { existsSync } from 'fs'
+import { downloadBinary, binName, binPath, cachePath } from './utils'
+import { existsSync, copyFileSync, chmodSync } from 'fs'
+import { join } from 'path'
 ;(async () => {
-  const from = `https://github.com/Akumzy/node-fs-watcher-go/releases/download/v0.0.1/${binName}`
-  if (existsSync(binPath)) {
+  const from = `https://github.com/Akumzy/node-fs-watcher-go/releases/download/v0.0.2/${binName}`
+  let oldFile = join(cachePath, binName)
+  if (existsSync(oldFile)) {
+    copyFileSync(join(cachePath, binName), binPath)
+    chmodSync(binPath, '0775')
     return
   }
   console.log(`Platform: ${process.platform} Arch: ${process.arch} Download binary .....!`)
--- utils.ts
+++ utils.ts
@@ -1,10 +1,10 @@
 import { join, parse } from 'path'
 import { get } from 'https'
-import { createWriteStream, mkdirSync, existsSync, chmodSync } from 'fs'
-
+import { createWriteStream, mkdirSync, existsSync, chmodSync, copyFileSync } from 'fs'
+import pkg from './package.json'
 export const binName = `node-watcher_${process.platform}_${process.arch}${process.platform === 'win32' ? '.exe' : ''}`
 export const binPath = join(__dirname, '.bin', binName)
-
+export const cachePath = join(process.env.HOME, '.cache', 'node-watcher', pkg.binVersion)
 export function downloadBinary(from: string) {
   return new Promise((resolve, reject) => {
     const req = get(from)
@@ -16,7 +16,9 @@ export function downloadBinary(from: string) {
       }
       let dir = parse(binPath).dir
       if (!existsSync(dir)) mkdirSync(parse(binPath).dir)
-      res.pipe(createWriteStream(binPath)).on('close', () => {
+      if (!existsSync(cachePath)) mkdirSync(cachePath)
+      res.pipe(createWriteStream(join(cachePath, binName))).on('close', () => {
+        copyFileSync(join(cachePath, binName), binPath)
         chmodSync(binPath, '0775')
         resolve(true)
       })
